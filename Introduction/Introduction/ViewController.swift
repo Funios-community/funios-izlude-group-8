7558e2f3fbb29a777d5f8d768af2735a3a26c675
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-07-20 20:22:02 +0700

    izlude challenge xib storyboard autolayout

--- Introduction/Introduction/ViewController.swift
+++ Introduction/Introduction/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return myViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       showPopupAlertView(at: indexPath)
+    }
+}
+
+extension ViewController {
+    func myViewCell() -> UITableViewCell {
+        let cell = UITableViewCell()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 30))
+        view.backgroundColor = .blue
+        cell.addSubview(view)
+        return cell
+    }
+    
+    func showPopupAlertView(at index: IndexPath){
+        let message = "ini di index ke - \(index.row)"
+        let alert = UIAlertController(title: "Halo", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ok", style: .default))
+        self.present(alert, animated: true)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

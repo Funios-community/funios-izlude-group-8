12573a9d14e8db0323fabfc2e4fbaffc9cb43c0f
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-07-22 16:49:24 +0700

    add autolayout sigin

--- Introduction2/Introduction2/ViewController.swift
+++ Introduction2/Introduction2/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInTapped(_ sender: Any) {
+        print("signIn email: \(emailTextField.text)")
+        emailTextField.resignFirstResponder()
+    }
+    
 }
 

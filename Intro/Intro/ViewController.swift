20b1b750ae258829c4c04d61f5515419a1efd8d5
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-07-23 12:06:36 +0700

    intro

---
+++ Intro/Intro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro
+//
+//  Created by Tommy-amarbank on 23/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
